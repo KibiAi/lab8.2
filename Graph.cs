a5a86407af531ea798599247b3fb2bd7fd204794
KibiAi <vetal2004v@gmail.com>
2023-11-24 12:45:55 +0200

    Добавьте файлы проекта.

---
+++ Graph.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8._2
+{
+    public abstract class Graph
+    {
+        public abstract void Draw();
+    }
+
+    public class LineGraph : Graph
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Візуалізація лінійного графіка");
+        }
+    }
+
+    public class PostGraph : Graph
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Візуалізація стовпчикового графіка");
+        }
+    }
+
+    public class CircleChart : Graph
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Візуалізація кругової діаграми");
+        }
+    }
+
+    public abstract class GraphFactory
+    {
+        public abstract Graph CreateGraph();
+    }
+
+    public class LineGraphFactory : GraphFactory
+    {
+        public override Graph CreateGraph()
+        {
+            return new LineGraph();
+        }
+    }
+
+    public class BarGraphFactory : GraphFactory
+    {
+        public override Graph CreateGraph()
+        {
+            return new PostGraph();
+        }
+    }
+
+    public class PieChartFactory : GraphFactory
+    {
+        public override Graph CreateGraph()
+        {
+            return new CircleChart();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using lab8._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть тип графіка (Line, Bar, Pie):");
+        string graphType = Console.ReadLine();
+
+        GraphFactory factory;
+        switch (graphType)
+        {
+            case "Line":
+                factory = new LineGraphFactory();
+                break;
+            case "Bar":
+                factory = new BarGraphFactory();
+                break;
+            case "Pie":
+                factory = new PieChartFactory();
+                break;
+            default:
+                Console.WriteLine("Невідомий тип графіка");
+                return;
+        }
+
+        Graph graph = factory.CreateGraph();
+        graph.Draw();
+    }
+}
